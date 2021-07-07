5c1a661e3c5ece509a3314fe4447cf6fe2c221c7
VincentYeh-dev <vincent3314534@gmail.com>
2021-07-08 03:49:56 +0800

    Add example "Scan"

---
+++ examples/Scan/Scan.ino
@@ -0,0 +1,21 @@
+#include "max7219.h"
+
+// Din pin : D10
+// CS pin  : D12
+// CLK pin : D11
+// Number of Max7219 : 2
+MAX7219 max7219(10,12,11,2);
+
+void setup() {
+  max7219.init();
+}
+
+void loop() {
+  for (int i = 0; i < 8; i++)
+  {
+    max7219.setDigit(0,i,0xff);
+    max7219.setDigit(1,i,0xff);
+    delay(1000);
+  }
+  max7219.clear();
+}
